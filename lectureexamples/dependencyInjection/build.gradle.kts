137c48727d32789acb0e79e8804b00de98261186
Patrick Stünkel <webminz@googlemail.com>
2023-09-04 12:23:19 +0200

    Spring context instead of spring core

--- lectureexamples/dependencyInjection/build.gradle.kts
+++ lectureexamples/dependencyInjection/build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework:spring-core:5.3.29")
+    implementation("org.springframework:spring-context:5.3.29")
     implementation("com.sun.xml.bind:jaxb-impl:2.3.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
