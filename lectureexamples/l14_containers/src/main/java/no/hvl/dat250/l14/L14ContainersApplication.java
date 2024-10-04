6ad12a74af104ea10893939f044284e34c91c809
Patrick Stünkel <webminz@googlemail.com>
2024-10-04 11:32:55 +0200

    adding database for the lecture example

--- lectureexamples/l14_containers/src/main/java/no/hvl/dat250/l14/L14ContainersApplication.java
+++ lectureexamples/l14_containers/src/main/java/no/hvl/dat250/l14/L14ContainersApplication.java
@@ -18,48 +18,4 @@ public class L14ContainersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(L14ContainersApplication.class, args);
 	}
-
-	@Bean
-	CommandLineRunner runner(@Autowired LocationRepository repository) {
-		return args -> {
-			Location bergen = new Location("bergen", 60.36, 5.35);
-
-			bergen.getObservations().add(new Observation(
-					bergen,
-					LocalDateTime.of(2024, 10, 2, 12, 0).toInstant(ZoneOffset.of("+2")),
-					14,
-					"degC",
-					Observation.ObservationType.TEMPERATURE));
-			bergen.getObservations().add(new Observation(
-					bergen,
-					LocalDateTime.of(2024, 10, 2, 12, 0).toInstant(ZoneOffset.of("+2")),
-					0,
-					"mm",
-					Observation.ObservationType.PRECIPITATION));
-			bergen.getObservations().add(new Observation(
-					bergen,
-					LocalDateTime.of(2024, 10, 2, 12, 0).toInstant(ZoneOffset.of("+2")),
-					4,
-					"m/s",
-					Observation.ObservationType.WIND));
-
-
-			bergen.getObservations().add(new Observation(
-					bergen,
-					LocalDateTime.of(2024, 10, 2, 12, 0).toInstant(ZoneOffset.of("+2")),
-					13,
-					"degC",
-					Observation.ObservationType.TEMPERATURE));
-
-			bergen.getObservations().add(new Observation(
-					bergen,
-					LocalDateTime.of(2024, 10, 2, 12, 0).toInstant(ZoneOffset.of("+2")),
-					2,
-					"m/s",
-					Observation.ObservationType.WIND));
-
-			repository.save(bergen);
-		};
-	}
-
 }
