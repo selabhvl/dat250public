396b6acc4d733756adf0bb8bc44ed98439d3d621
Patrick Stünkel <webminz@googlemail.com>
2025-10-10 08:04:34 +0200

    some polishing

--- lectureexamples/l15_containers/build.gradle.kts
+++ lectureexamples/l15_containers/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	runtimeOnly("com.h2database:h2")
+	//runtimeOnly("org.postgresql:postgresql")
 	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
 
